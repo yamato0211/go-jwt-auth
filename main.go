be262eb339196962df05b0403a7d083f526fcdfc
yamato0211 <kumayama0211@gmail.com>
2022-07-22 11:23:19 +0900

    [add] jwtToken作成

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"jwt-tutorial/auth"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +14,23 @@ func main() {
 			"messaga": "Hello Golang!",
 		})
 	})
+
+	// func Private(){
+	// 	post := &post{
+	// 		Title: "VGolangとGoogle Cloud Vision APIで画像から文字認識するCLIを速攻でつくる",
+	// 		Tag:   "Go",
+	// 		URL:   "https://qiita.com/po3rin/items/bf439424e38757c1e69b",
+	// 	}
+	// 	json.NewEncode(w).Encode(post)
+	// }
+
+	r.GET("/auth", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"token": auth.GenerateToken,
+		})
+	})
+
+	//r.GET("/private", auth.JwtMiddleware.)
 
 	r.Run(":8000")
 }
